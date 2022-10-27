6ed8799c5da056e8d4e84e35c31715d836908f6c
Nykaela Dodson <78758338+nykaeladodson@users.noreply.github.com>
2022-10-26 19:59:40 -0700

    working wednesday

---
+++ prisma/migrations/20221027012433_update_collection_name/migration.sql
@@ -0,0 +1,26 @@
+-- DropForeignKey
+ALTER TABLE "Monitor" DROP CONSTRAINT "Monitor_collectionId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Request" DROP CONSTRAINT "Request_collectionId_fkey";
+
+-- AlterTable
+ALTER TABLE "Assertion" ALTER COLUMN "requestId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "AssertionResult" ALTER COLUMN "responseId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "CollectionRun" ALTER COLUMN "collectionId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Monitor" ALTER COLUMN "collectionId" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Request" ALTER COLUMN "collectionId" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Monitor" ADD CONSTRAINT "Monitor_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE SET NULL ON UPDATE CASCADE;
