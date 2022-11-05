e0636ec39bbfe7d4373cc1ee20bbdb870319228d
Katherine Ebel <kathyebel@me.com>
2022-11-04 21:57:31 -0400

    use prisma query to fetch requests for collection runner
    Co-authored-by: Nykaela Dodson <nykaelad@gmail.com
    Co-authored-by: Gagan Sapkota <here.gagan@gmail.com>
    Co-authored-by: Hans Elde <hanserik.elde@gmail.com>

--- src/services/collectionServices.ts
+++ src/services/collectionServices.ts
@@ -1,38 +1,15 @@
-import { GraphQLClient, gql } from "graphql-request";
-
-const endpoint = process.env.SERVER_URL;
-const graphQLClient = new GraphQLClient(endpoint);
+import { PrismaClient } from '@prisma/client'
+const prismaClient = new PrismaClient()
 
 export const getCollectionData = async (collectionId) => {
-  let query = gql`
-    query Query($where: RequestWhereInput) {
-      requests(where: $where) {
-        id
-        title
-        createdAt
-        url
-        method
-        headers
-        body
-        stepNumber
-        assertions {
-          id
-          property
-          comparison
-          expected
-        }
-      }
-    }
-  `;
-
-  const queryVariables = {
+  return prismaClient.request.findMany({
     where: {
       collectionId: {
         equals: collectionId,
       },
     },
-  };
-
-  const data = await graphQLClient.request(query, queryVariables);
-  return data;
-};
+    include: {
+      assertions: true,
+    },
+  })
+}
