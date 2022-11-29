18256955952cc37fbf30436e7a1acdc120c860cb
Gagan Sapkota <here.gagan@gmail.com>
2022-11-29 09:12:02 -0800

    removed old migrations files
    Co-Authored-By: Katherine Ebel <kathyebel@me.com>
    Co-Authored-By: Nykaela Dodson <nykaelad@gmail.com>
    Co-Authored-By: Hans Elde <hanserik.elde@gmail.com>

--- prisma/migrations/20221117140021_add_enabled_to_monitor/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Monitor" ADD COLUMN     "enabled" BOOLEAN NOT NULL DEFAULT true;
--- prisma/migrations/20221127172533_update_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "AssertionResult_id_responseId_idx";
-
--- CreateIndex
-CREATE INDEX "Assertion_id_requestId_idx" ON "Assertion"("id", "requestId");
-
--- CreateIndex
-CREATE INDEX "AssertionResult_id_assertionId_responseId_idx" ON "AssertionResult"("id", "assertionId", "responseId");
