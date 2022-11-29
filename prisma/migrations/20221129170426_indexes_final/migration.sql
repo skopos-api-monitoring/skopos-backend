6c395421b782bf8476190e96de544ca0879381db
Gagan Sapkota <here.gagan@gmail.com>
2022-11-29 09:07:57 -0800

    updated the indexes to use on different foreign key columns
    -removed the combined indexes since we are not relying on them
    
    Co-Authored-By: Katherine Ebel <kathyebel@me.com>
    Co-Authored-By: Nykaela Dodson <nykaelad@gmail.com>
    Co-Authored-By: Hans Elde <hanserik.elde@gmail.com>

---
+++ prisma/migrations/20221129170426_indexes_final/migration.sql
@@ -0,0 +1,131 @@
+-- CreateTable
+CREATE TABLE "Collection" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "monitorId" INTEGER,
+
+    CONSTRAINT "Collection_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CollectionRun" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "collectionId" INTEGER NOT NULL,
+
+    CONSTRAINT "CollectionRun_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Request" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "url" TEXT NOT NULL,
+    "method" TEXT NOT NULL,
+    "headers" JSONB,
+    "body" JSONB,
+    "stepNumber" INTEGER NOT NULL,
+    "collectionId" INTEGER,
+
+    CONSTRAINT "Request_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Assertion" (
+    "id" SERIAL NOT NULL,
+    "property" TEXT NOT NULL,
+    "comparison" TEXT NOT NULL,
+    "expected" TEXT NOT NULL,
+    "requestId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Assertion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Response" (
+    "id" SERIAL NOT NULL,
+    "status" INTEGER NOT NULL,
+    "headers" JSONB NOT NULL,
+    "body" JSONB,
+    "latency" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "collectionRunId" INTEGER,
+    "requestId" INTEGER,
+
+    CONSTRAINT "Response_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AssertionResult" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "responseId" INTEGER,
+    "assertionId" INTEGER,
+    "pass" BOOLEAN NOT NULL,
+    "actual" TEXT NOT NULL,
+
+    CONSTRAINT "AssertionResult_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Monitor" (
+    "id" SERIAL NOT NULL,
+    "schedule" TEXT NOT NULL,
+    "contactInfo" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "snsTopicArn" TEXT,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+
+    CONSTRAINT "Monitor_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Collection_monitorId_idx" ON "Collection"("monitorId");
+
+-- CreateIndex
+CREATE INDEX "CollectionRun_collectionId_idx" ON "CollectionRun"("collectionId");
+
+-- CreateIndex
+CREATE INDEX "Request_collectionId_idx" ON "Request"("collectionId");
+
+-- CreateIndex
+CREATE INDEX "Assertion_requestId_idx" ON "Assertion"("requestId");
+
+-- CreateIndex
+CREATE INDEX "Response_collectionRunId_idx" ON "Response"("collectionRunId");
+
+-- CreateIndex
+CREATE INDEX "Response_requestId_idx" ON "Response"("requestId");
+
+-- CreateIndex
+CREATE INDEX "AssertionResult_responseId_idx" ON "AssertionResult"("responseId");
+
+-- CreateIndex
+CREATE INDEX "AssertionResult_assertionId_idx" ON "AssertionResult"("assertionId");
+
+-- AddForeignKey
+ALTER TABLE "Collection" ADD CONSTRAINT "Collection_monitorId_fkey" FOREIGN KEY ("monitorId") REFERENCES "Monitor"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CollectionRun" ADD CONSTRAINT "CollectionRun_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Request" ADD CONSTRAINT "Request_collectionId_fkey" FOREIGN KEY ("collectionId") REFERENCES "Collection"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Assertion" ADD CONSTRAINT "Assertion_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_collectionRunId_fkey" FOREIGN KEY ("collectionRunId") REFERENCES "CollectionRun"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Response" ADD CONSTRAINT "Response_requestId_fkey" FOREIGN KEY ("requestId") REFERENCES "Request"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AssertionResult" ADD CONSTRAINT "AssertionResult_responseId_fkey" FOREIGN KEY ("responseId") REFERENCES "Response"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AssertionResult" ADD CONSTRAINT "AssertionResult_assertionId_fkey" FOREIGN KEY ("assertionId") REFERENCES "Assertion"("id") ON DELETE CASCADE ON UPDATE CASCADE;
